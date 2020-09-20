dd0efc2d153017489310cbb7384b0dccaa6e1279
rolorin <roshan.bista@foodpanda.com>
2020-09-21 00:04:37 +0545

    Implemented get function

--- functions/get/handler.go
+++ functions/get/handler.go
@@ -1,5 +1,24 @@
-package get
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/rolorin/pd-apollo-loki/config"
+	"github.com/rolorin/pd-apollo-loki/utility/path"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/rolorin/pd-apollo-loki/responders/get"
+)
+
+// Handler for http...
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	responsePath := path.GetResponsePath(config.GetConfig("mockDirectory"), request.HTTPMethod, request.Path)
+	response := get.Response(responsePath)
+	res, _ := json.Marshal(response)
+	return events.APIGatewayProxyResponse{Body: string(res), StatusCode: 200}, nil
+}
 
 func main() {
-
+	lambda.Start(Handler)
 }
