8a24d97e93950dbfbb18fc2ed3795169570aa42f
rolorin <roshan.bista@foodpanda.com>
2020-09-21 00:05:28 +0545

    Completed post handler

--- functions/post/handler.go
+++ functions/post/handler.go
@@ -1,5 +1,24 @@
-package post
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/rolorin/pd-apollo-loki/config"
+	"github.com/rolorin/pd-apollo-loki/utility/path"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/rolorin/pd-apollo-loki/responders/post"
+)
+
+// Handler for http...
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	responsePath := path.GetResponsePath(config.GetConfig("mockDirectory"), request.HTTPMethod, request.Path)
+	response := post.Response(responsePath)
+	res, _ := json.Marshal(response)
+	return events.APIGatewayProxyResponse{Body: string(res), StatusCode: 200}, nil
+}
 
 func main() {
-
+	lambda.Start(Handler)
 }
