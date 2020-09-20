7148108a5b094703c3d85cbf4635d0ca7a8ede2d
rolorin <roshan.bista@foodpanda.com>
2020-09-21 00:03:34 +0545

    Added get responder

---
+++ responders/get/get.go
@@ -0,0 +1,11 @@
+package get
+
+import (
+	jsonreader "github.com/rolorin/pd-apollo-loki/utility/jsonReader"
+	latencygenerator "github.com/rolorin/pd-apollo-loki/utility/latencyGenerator"
+)
+
+// Response responder...
+func Response(filePath string) interface{} {
+	return latencygenerator.Sleep(jsonreader.JSONReader(filePath))
+}
