95b65e4f43b44eb6ff56236c0f9652f16d088a3e
rolorin <roshan.bista@foodpanda.com>
2020-09-21 00:05:16 +0545

    Completed patch handler

--- functions/patch/handler.go
+++ functions/patch/handler.go
@@ -1,5 +1,24 @@
-package patch
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/rolorin/pd-apollo-loki/config"
+	"github.com/rolorin/pd-apollo-loki/utility/path"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/rolorin/pd-apollo-loki/responders/patch"
+)
+
+// Handler for http...
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	responsePath := path.GetResponsePath(config.GetConfig("mockDirectory"), request.HTTPMethod, request.Path)
+	response := patch.Response(responsePath)
+	res, _ := json.Marshal(response)
+	return events.APIGatewayProxyResponse{Body: string(res), StatusCode: 200}, nil
+}
 
 func main() {
-
+	lambda.Start(Handler)
 }
