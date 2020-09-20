01fcd1167c6f80efe18bca5314fc2eb6861ce8fa
rolorin <roshan.bista@foodpanda.com>
2020-09-21 00:06:57 +0545

    Refactored latency to its own module

--- utility/jsonReader/jsonReader.go
+++ utility/jsonReader/jsonReader.go
@@ -4,31 +4,31 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"time"
 
-	latencycalculator "github.com/rolorin/pd-apollo-loki/utility/latencyCalculator"
+	latencygenerator "github.com/rolorin/pd-apollo-loki/utility/latencyGenerator"
 )
 
 // Latency struct is struct for Latency specified in the json file of response
-type Latency struct {
-	P95 float64 `json:"p95"`
-	P99 float64 `json:"p99"`
-}
+
 type jsonStruct struct {
-	Latency  Latency
+	Latency  latencygenerator.Latency
 	Response interface{} `json:"response"`
 }
 
 // JSONReader reads json from filename passed to it and responds with latency specified in file
-func JSONReader(filename string) interface{} {
+func JSONReader(filename string) (latencygenerator.Latency, interface{}) {
 	file, err := ioutil.ReadFile(filename)
+	zeroLatency := latencygenerator.Latency{
+		P95: float64(0),
+		P99: float64(0),
+	}
 
 	JSON := jsonStruct{}
 
 	if err != nil {
 		fmt.Println(err.Error())
 		fmt.Println("Error: Returning empty json and 0 latency")
-		return "{}"
+		return zeroLatency, "{}"
 	}
 
 	err = json.Unmarshal(file, &JSON)
@@ -36,12 +36,7 @@ func JSONReader(filename string) interface{} {
 	if err != nil {
 		fmt.Println(err.Error())
 		fmt.Println("Error: Returning empty json and 0 latency")
-		return "{}"
+		return zeroLatency, "{}"
 	}
-	latency := JSON.Latency
-	fmt.Println(latency.P95, latency.P99)
-	microseconds := (latencycalculator.Calculate(latency.P95, latency.P99) * int(1000))
-	sleep := time.Duration(microseconds)
-	time.Sleep(sleep)
-	return JSON.Response
+	return JSON.Latency, JSON.Response
 }
