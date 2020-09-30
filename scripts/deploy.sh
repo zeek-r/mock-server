82a163ccc1fd5c295bcd265b3b846e355a3432b2
rolorin <roshan.bista@foodpanda.com>
2020-09-30 23:10:45 +0545

    Added deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+while getopts ":s:" o; do
+    case "${o}" in
+    s)
+        STAGE=${OPTARG}
+        ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+set -x
+set -e
+
+echo "Deploying to AWS region: $REGION"
+sls deploy -v --region "$REGION" --stage "$STAGE" --user apollo
+
+bash send-to-datafridge.bash -r "$REGION"
